f2a63f9b01c5dcea5f79226f23fe77e1c79b16ff
KojiK222 <68413995+KojiK222@users.noreply.github.com>
2020-12-15 20:24:27 +0900

    photos_details_environment

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,12 +3,25 @@ class PhotosController < ApplicationController
   end
 
   def show
+    @photo = Photo.find(params[:id])
   end
 
   def new
     @photo = Photo.new
   end
 
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.user_id = current_user.id
+    @photo.save
+    redirect_to photo_path(@photo)
+  end
+
   def edit
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :body, :image)
   end
 end
