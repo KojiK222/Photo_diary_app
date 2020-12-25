7663e577358c1e5c5398ae52e173f18c9b9f523a
KojiK222 <68413995+KojiK222@users.noreply.github.com>
2020-12-25 10:26:57 +0900

    favorite.rb

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :photo
 
-  validates_uniquness_of :photo_id, scope: :user_id
+  validates_uniqueness_of :photo_id, scope: :user_id
 end
