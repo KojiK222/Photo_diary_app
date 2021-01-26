0102ea87377d0c06f3cd50d6819c8c25ba385697
KojiK222 <68413995+KojiK222@users.noreply.github.com>
2021-01-26 23:42:20 +0900

    DM_user_controller_haml_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
 
   def index
     @users = User.all
@@ -6,6 +7,23 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.talkroom_id == u.talkroom_id then
+            @isTalkroom = true
+            @talkroomId = cu.talkroom_id
+          end
+        end
+      end
+      if @isTalkroom
+      else
+        @Talkroom = Talkroom.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def edit
