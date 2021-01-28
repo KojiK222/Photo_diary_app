38ddc40b7649ff0312d8cb69cead5332d53b1eba
KojiK222 <68413995+KojiK222@users.noreply.github.com>
2021-01-28 23:36:27 +0900

    DM_user_controller_haml_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
+    @talkroom = Talkroom.new
     unless @user.id == current_user.id
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
